2a2db17eaf5b7c8372988f5bfce2c7f0e4e0b11c
49659410+tx0c <>
2024-04-22 19:49:38 +0000

    feat(auto-pr): update setup-node for auto create assets-pr

--- bin/sync-up-latest-round.mjs
+++ bin/sync-up-latest-round.mjs
@@ -7,33 +7,42 @@ const client = new S3Client() // use credentials from environment
 
 const AssetsDir = './public/static'
 
-export const main = async () => {
+export const main = async (branch = 'prod') => {
   const controller = new AbortController()
   const { signal } = controller
+  const developPrefix = branch !== 'prod' ? 'web-develop/' : ''
 
+  console.log('```console')
   try {
     // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
     const roundsContent = await (
       await client.send(
         new GetObjectCommand({
           Bucket: 'matters-billboard',
-          Key: 'rounds/rounds.json',
+          Key: `${developPrefix}rounds/rounds.json`,
         })
       )
     ).Body.transformToString()
     const rounds = JSON.parse(roundsContent)
+    const lastRound = rounds[rounds.length - 1]
+
     console.log(
       new Date(),
       `got ${rounds.length} rounds with latest:`,
-      rounds[rounds.length - 1]
+      lastRound
     )
-    await writeFile(`${AssetsDir}/rounds.json`, roundsContent, { signal })
+    if (lastRound.draft) {
+      delete lastRound.draft // finalize the last round if still draft
+      // roundsContent = JSON.stringify(rounds);
+    }
+
+    await writeFile(`${AssetsDir}/rounds.json`, JSON.stringify(rounds), {
+      signal,
+    })
     console.log(
       new Date(),
       `written rounds.json with ${(roundsContent.length / 1024).toFixed(1)} KBytes.`
     )
-
-    const lastRound = rounds[rounds.length - 1]
 
     console.log(new Date(), 'latest round:', lastRound.id, lastRound.dirpath)
 
@@ -47,7 +56,7 @@ export const main = async () => {
         await client.send(
           new GetObjectCommand({
             Bucket: 'matters-billboard',
-            Key: `rounds/${lastRound.dirpath}/${file}`,
+            Key: `${developPrefix}rounds/${lastRound.dirpath}/${file}`,
           })
         )
       ).Body.transformToString()
@@ -66,6 +75,9 @@ export const main = async () => {
   } catch (err) {
     console.error(new Date(), `ERROR:`, err)
   }
+
+  console.log('```')
 }
 
-await main()
+// console.log('run with argv:', process.argv)
+await main(process.argv?.[2] || 'prod')
