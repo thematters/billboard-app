3530f08a926846ec8a8aff790b68eefbb6c5e23d
49659410+tx0c <>
2024-04-22 19:49:38 +0000

    feat(auto-pr): update setup-node for auto create assets-pr

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
+[![Billboard Rounds - Create PR to update latest round](https://github.com/thematters/billboard-app/actions/workflows/checkin-assets.yaml/badge.svg)](https://github.com/thematters/billboard-app/actions/workflows/checkin-assets.yaml)
+
 ## Getting started
 
 ### Start local dev
--- bin/sync-up-latest-round.mjs
+++ bin/sync-up-latest-round.mjs
@@ -7,33 +7,47 @@ const client = new S3Client() // use credentials from environment
 
 const AssetsDir = './public/static'
 
-export const main = async () => {
+const outputFile = process.env.GITHUB_OUTPUT || './output-vars'
+
+export const main = async (branch = 'prod') => {
   const controller = new AbortController()
   const { signal } = controller
+  const developPrefix = branch !== 'prod' ? 'web-develop/' : ''
 
+  console.log('```console')
   try {
     // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
-    const roundsContent = await (
+    let roundsContent = await (
       await client.send(
         new GetObjectCommand({
           Bucket: 'matters-billboard',
-          Key: 'rounds/rounds.json',
+          Key: `${developPrefix}rounds/rounds.json`,
         })
       )
     ).Body.transformToString()
     const rounds = JSON.parse(roundsContent)
+    const lastRound = rounds[rounds.length - 1]
+    const output_vars = {}
+
     console.log(
       new Date(),
       `got ${rounds.length} rounds with latest:`,
-      rounds[rounds.length - 1]
+      lastRound
     )
+    output_vars.round_id = lastRound.id
+
+    if (lastRound.draft) {
+      output_vars.draft = lastRound.draft
+      delete lastRound.draft // finalize the last round if still draft
+      roundsContent =
+        '[ ' + rounds.map((r) => JSON.stringify(r)).join(',\n') + ' ]'
+    }
+
     await writeFile(`${AssetsDir}/rounds.json`, roundsContent, { signal })
     console.log(
       new Date(),
       `written rounds.json with ${(roundsContent.length / 1024).toFixed(1)} KBytes.`
     )
-
-    const lastRound = rounds[rounds.length - 1]
 
     console.log(new Date(), 'latest round:', lastRound.id, lastRound.dirpath)
 
@@ -47,7 +61,7 @@ export const main = async () => {
         await client.send(
           new GetObjectCommand({
             Bucket: 'matters-billboard',
-            Key: `rounds/${lastRound.dirpath}/${file}`,
+            Key: `${developPrefix}rounds/${lastRound.dirpath}/${file}`,
           })
         )
       ).Body.transformToString()
@@ -63,9 +77,21 @@ export const main = async () => {
         `written ${file} with ${(fileContent.length / 1024).toFixed(1)} KBytes.`
       )
     }
+
+    const outputContent = Object.entries(output_vars)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('\n')
+    await writeFile(outputFile, outputContent, { signal })
+    console.log(
+      new Date(),
+      `written output variables ${outputContent.length} Bytes:\n${outputContent}\ninto "${outputFile}"`
+    )
   } catch (err) {
     console.error(new Date(), `ERROR:`, err)
   }
+
+  console.log('```')
 }
 
-await main()
+// console.log('run with argv:', process.argv)
+await main(process.argv?.[2] || 'prod')
