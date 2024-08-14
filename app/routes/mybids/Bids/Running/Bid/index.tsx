2f6d18800e3f9f50440589e875d6f784059bd0eb
Zeck Li <11781254+zeckli@users.noreply.github.com>
2024-08-14 21:37:13 +0800

    feat(ui): update running list layout

--- app/routes/mybids/Bids/Running/Bid/index.tsx
+++ app/routes/mybids/Bids/Running/Bid/index.tsx
@@ -6,6 +6,8 @@ import SvgEye from '@svg/Eye'
 import SvgEyeCross from '@svg/EyeCross'
 import { formatRoundId } from '@util/format'
 
+import Label from './Label'
+
 type Props = {
   state: string
   data: Record<string, any>
@@ -14,8 +16,6 @@ type Props = {
 const Bid = ({ state, data }: Props) => {
   const navigate = useNavigate()
   const { board, bid, epoch, displayRange } = data
-  const isRunning = state === 'running'
-  const isUpcoming = state === 'upcoming'
 
   const onEdit = () => {
     const { id } = board
@@ -28,34 +28,20 @@ const Bid = ({ state, data }: Props) => {
   }
 
   const baseCss = 'p-5 bg-black t-14 md:t-18'
-  const gridCss = 'cols-1 gap-y-2 md:cols-8 md:gap-y-0 md:gap-x-3'
+  const gridCss = 'cols-1 gap-y-2 md:cols-10 md:gap-y-0 md:gap-x-3'
   const rowCss = clsx(baseCss, gridCss, 'b-b-dashed border-beige/30')
-  const labelCss =
-    'py-1 px-2 w-fit md:w-full f-center-start rounded-lg t-16 text-black'
-  const runningCss = clsx(labelCss, 'bg-[#91d7ff]')
-  const upcomingCss = clsx(labelCss, 'bg-[#fffc62]')
   const cellCss = 'font-semibold f-center-start'
-  const timeCss = 'md:col-span-4 text-beige/60 f-center-start'
+  const nameCss = clsx(cellCss, 'pl-2 md:col-span-2')
+  const timeCss = 'md:col-span-5 text-beige/60 f-center-start'
   const actionCss = 'f-center-start md:f-center-end'
   const editCss = 'w-fit text-grass f-center-end cursor-pointer'
 
   return (
     <section className={rowCss}>
       <div className="f-center-start">
-        {isRunning && (
-          <div className={runningCss}>
-            <SvgEye css="mr-1" />
-            Running
-          </div>
-        )}
-        {isUpcoming && (
-          <div className={upcomingCss}>
-            <SvgEyeCross css="mr-1" />
-            Upcoming
-          </div>
-        )}
+        <Label state={state} />
       </div>
-      <div className={cellCss}>{board.name}</div>
+      <div className={nameCss}>{board.name}</div>
       <div className={cellCss}>No. {formatRoundId(`${epoch}`)}</div>
       <div className={timeCss}>
         Display:&nbsp;
