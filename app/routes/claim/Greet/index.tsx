fd4cb551813a0db4f4784235825ecaebe5aac4d3
Zeck Li <11781254+zeckli@users.noreply.github.com>
2024-11-04 16:35:49 +0800

    feat(copies): add the guide link for the creator rewards

--- app/routes/claim/Greet/index.tsx
+++ app/routes/claim/Greet/index.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from '@remix-run/react'
+
 import BaseButton from '@component/Button/Base'
 import SvgClaim from '@svg/Claim'
 
@@ -17,7 +19,16 @@ const Greet = ({ open }: Props) => {
       <p className={contentCss}>
         The tax revenue from billboards is periodically distributed to creators
         through the quadratic funding mechanism. Connect your wallet to check
-        your eligibility for claiming rewards.
+        your eligibility for claiming rewards. If you're claiming rewards for
+        the first time, please follow our
+        <NavLink
+          className="text-grass"
+          to="https://matters.town/a/bnrzg4x1iren"
+          target="_blank"
+        >
+          &nbsp;step-by-step guide&nbsp;
+        </NavLink>
+        to get started.
       </p>
       <SvgClaim css={svgCss} />
       <BaseButton css={btnCss} color="dim" click={open}>
