1f2693d8a691769ff60bd0fead9fa952b4644e68
Zeck Li <11781254+zeckli@users.noreply.github.com>
2025-04-12 21:11:55 +0800

    retire all components and pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,49 +1,5 @@
-import type { LinksFunction } from '@remix-run/node'
-import type { AppEnvs } from '@type'
-
-import {
-  LiveReload,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from '@remix-run/react'
-import { json } from '@remix-run/node'
-
-import AnaltyicsContext from '@component/Context/Analytics'
-import EnvsContext from '@component/Context/Envs'
-import WalletContext from '@component/Context/Wallet'
-import Doc from '@component/Doc'
-import Layout from '@component/Layout'
-import { readEnvs } from '@util/envs'
-
-import styles from './main.css'
-
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
-
-export const loader = async () => {
-  const envs = readEnvs()
-  return json(envs)
-}
-
 const App = () => {
-  const envs = useLoaderData() as AppEnvs
-  return (
-    <EnvsContext envs={envs}>
-      <WalletContext projectId={envs.idWalletConnect}>
-        <Doc gaId={envs.gaId}>
-          <AnaltyicsContext gaId={envs.gaId}>
-            <Layout>
-              <Outlet context={envs} />
-            </Layout>
-            <Scripts />
-            <ScrollRestoration />
-            <LiveReload />
-          </AnaltyicsContext>
-        </Doc>
-      </WalletContext>
-    </EnvsContext>
-  )
+  return ()
 }
 
 export default App
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,46 +1,9 @@
-import type { Config } from 'tailwindcss'
-
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    screens: {
-      sm: "540px",
-      md: "768px",
-      lg: "1104px",
-    },
-    colors: {
-      black: "#333333",
-      gray: "#b3b3b3",
-      green: "#80AB01",
-      white: "#ffffff",
-      red: '#FF6262',
-    },
-    extend: {
-      fontFamily: {
-        cyber: ["Saved By Zero", "Space Grotesk", "sans-serif"],
-        space: ["Space Grotesk", "sans-serif"],
-      },
-      colors: {
-        beige: "#ede8d6",
-        dark: "#000000",
-        oak: "#4f4f4f",
-        dim: "#212121",
-        grass: "#c3f432",
-        lemon: "#b1e41d",
-        olive: "#2f3e07",
-        steam: "#dddddd",
-      },
-      width: {
-        modal: "375px",
-      },
-      maxWidth: {
-        main: "1104px",
-        modal: "375px",
-      },
-      minWidth: {
-        main: "375px",
-      },
-    },
+    screens: {},
+    colors: {},
+    extend: {},
   },
   plugins: [],
-} satisfies Config
+}
