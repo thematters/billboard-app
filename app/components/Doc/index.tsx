28aada31c85bfdbf4bec3a7ae42c20c200669cf5
Zeck Li <11781254+zeckli@users.noreply.github.com>
2025-07-20 00:02:20 +0800

    add 404 page

--- app/components/Doc/index.tsx
+++ app/components/Doc/index.tsx
@@ -15,6 +15,7 @@ const Doc = ({ children }: ComponentPropsType) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <link rel="icon" href="favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
--- app/routes/$/index.tsx
+++ app/routes/$/index.tsx
@@ -1,5 +1,18 @@
+import clsx from 'clsx'
+
+import Box from '@components/Box'
+
 const Page = () => {
-  return <p>Not Found</p>
+  const baseCss = clsx('main-min-max mx-auto py-10 md:py-20')
+  const titleCss = clsx('pt-8 section-title pb-4')
+  const textCss = clsx('text-center')
+
+  return (
+    <Box classes={baseCss}>
+      <h1 className={titleCss}>404</h1>
+      <p className={textCss}>page not found</p>
+    </Box>
+  )
 }
 
 export default Page
